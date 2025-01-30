0e4c84977a7db888d1122ef3a964e8c34119ecbd
ericKwon95 <ericyong95@gmail.com>
2025-01-30 10:58:40 +0900

    [Feat] Book 모델 생성

---
+++ BookKitty/BookKitty/Source/Common/Model/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  BookKitty
+//
+//  Created by 권승용 on 1/29/25.
+//
+
+import Foundation
+
+struct Book {
+    let isbn: String
+    let title: String
+    let author: String
+    let publisher: String
+    let thumbnailUrl: URL?
+}
