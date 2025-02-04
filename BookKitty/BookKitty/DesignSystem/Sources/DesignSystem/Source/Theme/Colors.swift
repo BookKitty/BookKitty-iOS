fa48ba18300ea055e4437eca62a30dd77fc6d0dd
seongto <imseongto@gmail.com>
2025-02-04 09:42:52 +0900

    [Feat] 본 프로젝트에 Design System import 추가 외 디자인 기본 세팅
    - 컬러 에셋 파일 생성
    - 폰트 추가

---
+++ BookKitty/BookKitty/DesignSystem/Sources/DesignSystem/Source/Theme/Colors.swift
@@ -0,0 +1,80 @@
+//
+//  Colors.swift
+//  DesignSystem
+//
+//  Created by MaxBook on 1/29/25.
+//  기본적으로 컬러는 Colors 에셋 파일에서 직접 가져다 쓰는 것을 원칙으로 합니다.
+
+import UIKit
+
+/// 시스템 컬러는 검정과 프로젝트 맞춤 컬러 보라색 두가지만 사용합니다.
+public enum SystemColors: String {
+    case Black = "Black"
+}
+
+/// 컬러의 ramp 어레인지를 다음과 같이 제한합니다.
+public enum SystemColorRamps: String {
+    case r50 = "50"
+    case r100 = "100"
+    case r200 = "200"
+    case r300 = "300"
+    case r400 = "400"
+    case r500 = "500"
+    case r600 = "600"
+    case r700 = "700"
+    case r800 = "800"
+    case r900 = "900"
+}
+
+public enum Colors {
+    // MARK: - Background Color
+
+    public static var background0: UIColor { UIColor.designSystemColor(name: "Background0") }
+    public static var background1: UIColor { UIColor.designSystemColor(name: "Background1") }
+    public static var background2: UIColor { UIColor.designSystemColor(name: "Background2") }
+    public static var backgroundModal: UIColor { UIColor.designSystemColor(name: "BackgroundModal")
+    }
+
+    // MARK: - Brand
+
+    public static var brandMain: UIColor { UIColor.designSystemColor(name: "BrandMain") }
+    public static var brandMain30: UIColor { UIColor.designSystemColor(name: "BrandMain30") }
+    public static var brandSub: UIColor { UIColor.designSystemColor(name: "BrandSub") }
+    public static var brandSub30: UIColor { UIColor.designSystemColor(name: "BrandSub30") }
+
+    // MARK: - Font Color
+
+    public static var fontDisabled: UIColor { UIColor.designSystemColor(name: "FontDisabled") }
+    public static var fontMain: UIColor { UIColor.designSystemColor(name: "FontMain") }
+    public static var fontSub1: UIColor { UIColor.designSystemColor(name: "FontSub1") }
+    public static var fontSub2: UIColor { UIColor.designSystemColor(name: "FontSub2") }
+    public static var fontWhite: UIColor { UIColor.designSystemColor(name: "FontWhite") }
+    public static var fontWhiteDisabled: UIColor {
+        UIColor.designSystemColor(name: "FontWhiteDisabled")
+    }
+
+    public static var fontWhiteUnselected: UIColor {
+        UIColor.designSystemColor(name: "FontWhiteUnselected")
+    }
+
+    // MARK: - Shadow Color
+
+    public static var shadow15: UIColor { UIColor.designSystemColor(name: "Shadow15") }
+    public static var shadow25: UIColor { UIColor.designSystemColor(name: "Shadow25") }
+
+    // MARK: - Status Color
+
+    public static var statusGreen: UIColor { UIColor.designSystemColor(name: "StatusGreen") }
+    public static var statusRed: UIColor { UIColor.designSystemColor(name: "StatusRed") }
+    public static var statusYellow: UIColor { UIColor.designSystemColor(name: "StatusYellow") }
+
+    // MARK: - System Color
+
+    /// 숫자가 낮을수록 연하다.
+    /// `.Black-<ramp>`로 직접 확인 가능.
+    /// - Parameter ramp: 원하는 레벨의 진한 정도를 선택. 최대 900, 최소 50
+    /// - Returns: UIColor 값 반환.
+    public static func systemGray(_ ramp: SystemColorRamps) -> UIColor? {
+        UIColor.designSystemColor(name: "Black-\(ramp.rawValue)")
+    }
+}
---
+++ BookKitty/BookKitty/DesignSystem/Sources/DesignSystem/Source/Theme/Vars.swift
@@ -0,0 +1,83 @@
+//
+//  Vars.swift
+//  DesignSystem
+//
+//  Created by MaxBook on 1/29/25.
+//
+
+import UIKit
+
+/// 각종 사이즈 상수값 관리
+public enum Vars {
+    // MARK: - Padding Values
+
+    /// 레이아웃에서 패딩(inset) 영역 지정용 / large / 32
+    public static let paddingLarge: CGFloat = 32
+    /// 레이아웃에서 패딩(inset) 영역 지정용 / regular / 24
+    public static let paddingReg: CGFloat = 24
+    /// 레이아웃에서 패딩(inset) 영역 지정용 / small / 16
+    public static let paddingSmall: CGFloat = 16
+
+    // MARK: - button Values
+
+    /// 텍스트뷰와 같이 큰 컴포넌트의 높이값  / 160
+    public static let viewSizeHuge: CGFloat = 160
+    /// 버튼 등의 컴포넌트 사이즈 지정용. 대체로 높이값 / large / 72
+    public static let viewSizeLarge: CGFloat = 72
+    /// 버튼 등의 컴포넌트 사이즈 지정용. 대체로 높이값 / regular / 48
+    public static let viewSizeReg: CGFloat = 48
+    /// 버튼 등의 컴포넌트 사이즈 지정용. 대체로 높이값 / small / 40
+    public static let viewSizeSmall: CGFloat = 40
+    /// 버튼 등의 컴포넌트 사이즈 지정용. 대체로 높이값 / tiny / 24
+    public static let viewSizeTiny: CGFloat = 24
+
+    // MARK: - Radius Values
+
+    /// UI 컴포넌트의 모서리 라운딩 사이즈 / large / 36
+    public static let radiusLarge: CGFloat = 36
+    /// UI 컴포넌트의 모서리 라운딩 사이즈 / regular / 24
+    public static let radiusReg: CGFloat = 24
+    /// UI 컴포넌트의 모서리 라운딩 사이즈 / small / 20
+    public static let radiusSmall: CGFloat = 20
+    /// UI 컴포넌트의 모서리 라운딩 사이즈 / tiny / 12
+    public static let radiusTiny: CGFloat = 12
+    /// UI 컴포넌트의 모서리 라운딩 사이즈 / mini / 8
+    public static let radiusMini: CGFloat = 8
+
+    // MARK: - Item Spacing Values
+
+    /// UI 컴포넌트 간의 상하 거리 간격 조정용 사이즈 / 4
+    public static let spacing4: CGFloat = 4
+    /// UI 컴포넌트 간의 상하 거리 간격 조정용 사이즈 / 8
+    public static let spacing8: CGFloat = 8
+    /// UI 컴포넌트 간의 상하 거리 간격 조정용 사이즈 / 12
+    public static let spacing12: CGFloat = 12
+    /// UI 컴포넌트 간의 상하 거리 간격 조정용 사이즈 / 20
+    public static let spacing20: CGFloat = 20
+    /// UI 컴포넌트 간의 상하 거리 간격 조정용 사이즈 / 24
+    public static let spacing24: CGFloat = 24
+    /// UI 컴포넌트 간의 상하 거리 간격 조정용 사이즈 / 32
+    public static let spacing32: CGFloat = 32
+    /// UI 컴포넌트 간의 상하 거리 간격 조정용 사이즈 / 48
+    public static let spacing48: CGFloat = 48
+    /// UI 컴포넌트 간의 상하 거리 간격 조정용 사이즈 / 72
+    public static let spacing72: CGFloat = 72
+
+    // MARK: - Image values
+
+    /// 책 커버 이미지용 사이즈. 가로값 고정용 / 200
+    public static let imageFixedWidth: CGFloat = 200
+    /// 책 커버 이미지용 사이즈. 세로값 고정용 / 128
+    public static let imageFixedHeight: CGFloat = 128
+    /// 책 커버 이미지용 사이즈. 세로값 고정용 / 144
+    public static let imageFixedHeightLarge: CGFloat = 144
+
+    // MARK: - etc
+
+    /// 상단 네비게이션바 높이용 사이즈 / 56
+    public static let gnbHeight: CGFloat = 56
+
+    public static func setContainerInset(_ inset: CGFloat) -> UIEdgeInsets {
+        UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+    }
+}
