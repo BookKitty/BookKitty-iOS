30099787a74b2d7df1811b5582e940ea405264b9
seongto <imseongto@gmail.com>
2025-02-04 09:48:59 +0900

    [Feat] 각종 알러트뷰 컴포넌트 추가
    - 이미지가 포함된 알러트뷰를 3종류 추가.
    - 각 컴포넌트의 텍스트 등을 주입하여 초기화 가능하도록 구성

---
+++ BookKitty/BookKitty/DesignSystem/Sources/DesignSystem/Source/Component/UIView/ManageBookPopupView.swift
@@ -0,0 +1,128 @@
+//
+//  ManageBookPopupView.swift
+//  DesignSystem
+//
+//  Created by MaxBook on 2/3/25.
+//
+
+import SnapKit
+import Then
+import UIKit
+
+public enum ManageBookMode {
+    case add
+    case delete
+}
+
+@available(iOS 15.0, *)
+public class ManageBookPopupView: UIView {
+    // MARK: Lifecycle
+
+    public init(bookTitle: String = "책 제목", mode: ManageBookMode) {
+        self.bookTitle = bookTitle
+        self.mode = mode
+        super.init(frame: .zero)
+
+        setupViews()
+        setupProperties()
+        setupLayouts()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: Public
+
+    public let bookTitle: String
+    public let mode: ManageBookMode
+
+    public let cancelButton = RoundButton(title: "취소", isSecondary: true)
+    public let confirmButton = RoundButton()
+
+    public let buttonStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = Vars.spacing12
+        $0.alignment = .center
+        $0.distribution = .fillEqually
+    }
+
+    // MARK: Private
+
+    private let iconImageView = UIImageView().then {
+        $0.preferredSymbolConfiguration = .init(pointSize: Vars.viewSizeSmall, weight: .regular)
+        $0.backgroundColor = .clear
+    }
+
+    private let messageLabel = BodyLabel().then {
+        $0.textAlignment = .center
+    }
+
+    private let bookTitleLabel = BodyLabel(weight: .semiBold).then {
+        $0.textAlignment = .center
+        $0.textColor = Colors.brandSub
+    }
+}
+
+// MARK: - Setup UI
+
+@available(iOS 15.0, *)
+extension ManageBookPopupView {
+    private func setupViews() {
+        [
+            iconImageView,
+            messageLabel,
+            bookTitleLabel,
+            buttonStackView,
+        ].forEach { self.addSubview($0) }
+
+        [
+            cancelButton,
+            confirmButton,
+        ].forEach { buttonStackView.addArrangedSubview($0) }
+    }
+
+    private func setupProperties() {
+        backgroundColor = Colors.background0
+        layer.cornerRadius = Vars.radiusTiny
+        setBiggerShadow()
+
+        bookTitleLabel.text = bookTitle
+
+        switch mode {
+        case .add:
+            messageLabel.text = "나의 책장에 아래의 책을 등록하시겠습니까?"
+            iconImageView.image = UIImage(systemName: "trash.fill")
+            iconImageView.tintColor = Colors.statusRed
+        case .delete:
+            messageLabel.text = "나의 책장에서 아래의 책을 제외하시겠습니까?"
+            iconImageView.image = UIImage(systemName: "bookmark.fill")
+            iconImageView.tintColor = Colors.brandMain
+        }
+    }
+
+    private func setupLayouts() {
+        iconImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().inset(Vars.spacing48)
+        }
+
+        messageLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(Vars.paddingSmall)
+            make.top.equalTo(iconImageView.snp.bottom).offset(Vars.spacing24)
+        }
+
+        bookTitleLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(Vars.paddingSmall)
+            make.top.equalTo(messageLabel.snp.bottom).offset(Vars.spacing12)
+        }
+
+        buttonStackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(Vars.paddingSmall)
+            make.top.equalTo(bookTitleLabel.snp.bottom).offset(Vars.spacing48)
+            make.bottom.equalToSuperview().inset(Vars.paddingReg)
+            make.height.equalTo(Vars.viewSizeReg)
+        }
+    }
+}
