ced8cbdb3718b7a5fb655ef076b5b57c3fe3cb43
seongto <imseongto@gmail.com>
2025-02-05 12:19:02 +0900

    [Feat] 이미지뷰 컴포넌트 추가 및 변경
    - 플렉서블하게 너비값을 입력하면 자동으로 높이값을 지정해서 만들어주는 컴포넌트 추가
    - 기존 고정 높이값에 새로운 사이즈 추가.

--- BookKitty/BookKitty/DesignSystem/Sources/DesignSystem/Source/Component/UIImageView/FlexibleImageView.swift
+++ BookKitty/BookKitty/DesignSystem/Sources/DesignSystem/Source/Component/UIImageView/FlexibleImageView.swift
@@ -1,5 +1,5 @@
 //
-//  WidthFixedImageView.swift
+//  FlexibleImageView.swift
 //  DesignSystem
 //
 //  Created by MaxBook on 2/4/25.
@@ -8,34 +8,22 @@
 import SnapKit
 import UIKit
 
-public enum BookImageFixedWidth {
-    case regular
-    case large
-}
-
-public class WidthFixedImageView: UIImageView, ImageLoadableView {
+public class FlexibleImageView: UIImageView {
     // MARK: Lifecycle
 
     // MARK: - Initializer
 
-    /// 너비가 고정된 이미지뷰.
+    /// 가로값 기준 세로값이 정해지는 이미지뷰.
     /// 책의 이미지를 표시하는데 사용.
-    /// 두가지의 값(128, 144) 중 선택하여 사용합니다. ``BookImageFixedWidth`` 참고.
+    /// 가로값이 오토레이아웃으로 변경되는 경우에 사용.
     ///
     /// - Parameters:
     ///   - imageUrl: 사용하고자 하는 이미지의
-    public init(imageUrl: String = "", width: BookImageFixedWidth) {
+    public init(imageUrl: String = "", width: CGFloat) {
         self.imageUrl = imageUrl
-
-        switch width {
-        case .regular:
-            fixedWidth = Vars.imageFixedWidth
-        case .large:
-            fixedWidth = Vars.imageFixedWidthLarge
-        }
+        viewWidth = width
 
         super.init(frame: .zero)
-
         setupProperties()
     }
 
@@ -47,13 +35,12 @@ public class WidthFixedImageView: UIImageView, ImageLoadableView {
     // MARK: Public
 
     public var imageUrl: String
-    public var fixedWidth: CGFloat
-    public var onImageLoaded: (() -> Void)?
+    public var viewWidth: CGFloat
 }
 
 // MARK: - Setup UI
 
-extension WidthFixedImageView {
+extension FlexibleImageView {
     private func setupProperties() {
         contentMode = .scaleAspectFit
         clipsToBounds = true
@@ -74,13 +61,10 @@ extension WidthFixedImageView {
             if let imageSize = bookImage?.size {
                 let aspectRatio = imageSize.height / imageSize.width
                 snp.remakeConstraints { make in
-                    make.width.equalTo(fixedWidth) // 너비 고정
-                    make.height.equalTo(fixedWidth * aspectRatio) // 높이 자동 조정
+                    make.width.equalTo(viewWidth) // 너비 고정
+                    make.height.equalTo(viewWidth * aspectRatio) // 높이 자동 조정
                 }
             }
-
-            // 이미지 로딩 완료 후 콜백 실행
-            onImageLoaded?()
         }
     }
 }
