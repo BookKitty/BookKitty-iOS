7c2eefae274ecba6757b512ec43e650f474f2e56
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2025-02-06 21:14:30 +0900

    [Feat] HomeViewController 추가하기

--- BookKitty/BookKitty/Source/Coordinator/HomeCoordinator.swift
+++ BookKitty/BookKitty/Source/Coordinator/HomeCoordinator.swift
@@ -5,6 +5,7 @@
 //  Created by 전성규 on 1/26/25.
 //
 
+import RxSwift
 import UIKit
 
 protocol HomeCoordinator: Coordinator {
@@ -16,7 +17,9 @@ final class DefaultHomeCoordinator: Coordinator {
 
     init(_ navigationController: UINavigationController) {
         self.navigationController = navigationController
-        homeViewController = HomeViewController()
+        let repository = MockBookRepository()
+        homeViewModel = HomeViewModel(bookRepository: repository)
+        homeViewController = HomeViewController(viewModel: homeViewModel)
     }
 
     // MARK: Internal
@@ -27,5 +30,45 @@ final class DefaultHomeCoordinator: Coordinator {
     var navigationController: UINavigationController
     var homeViewController: HomeViewController
 
-    func start() {}
+    var homeViewModel: HomeViewModel
+
+    func start() { showHomeScreen() }
+
+    // MARK: Private
+
+    private let disposeBag = DisposeBag()
+}
+
+extension DefaultHomeCoordinator {
+    /// 책 목록 화면 표시
+    ///
+    /// 책 목록 화면을 생성하고 ViewModel과 ViewController를 연결
+    /// 사용자가 책을 선택하면 책 상세 화면으로 이동
+    private func showHomeScreen() {
+        let bookRepository = MockBookRepository()
+        let homeViewModel = HomeViewModel(bookRepository: bookRepository)
+        let homeViewController = HomeViewController(viewModel: homeViewModel)
+
+        // 책 상세 화면으로 이동 이벤트 처리
+        homeViewModel.navigateToBookDetail
+            .withUnretained(self)
+            .subscribe(onNext: { coordinator, book in
+                coordinator.showBookDetailScreen(with: book)
+            })
+            .disposed(by: disposeBag)
+
+        navigationController.pushViewController(homeViewController, animated: true)
+    }
+
+    /// 책 상세 화면 표시
+    ///
+    /// 책 상세 화면을 생성하고 ViewModel과 ViewController를 연결
+    /// 탭바를 숨기고 화면을 네비게이션 스택에 추가
+    /// - Parameter book: 책 상세 화면에서 표시할 Book 정보가 담긴 구조체
+    private func showBookDetailScreen(with _: Book) {
+        let bookDetailViewModel = BookDetailViewModel()
+        let bookDetailViewController = BookDetailViewController(viewModel: bookDetailViewModel)
+
+        navigationController.pushViewController(bookDetailViewController, animated: true)
+    }
 }
--- BookKitty/BookKitty/Source/Feature/Home/ViewModel/HomeViewModel.swift
+++ BookKitty/BookKitty/Source/Feature/Home/ViewModel/HomeViewModel.swift
@@ -1,6 +1,60 @@
-//
-//  HomeViewModel.swift
-//  BookKitty
-//
-//  Created by 권승용 on 1/23/25.
-//
+import Foundation
+import RxCocoa
+import RxSwift
+
+final class HomeViewModel: ViewModelType {
+    // MARK: Lifecycle
+
+    init(
+        bookRepository: BookRepository
+    ) {
+        self.bookRepository = bookRepository
+    }
+
+    // MARK: Internal
+
+    struct Input {
+        let viewDidLoad: Observable<Void> // 뷰가 로드될 때 전달받은 질문
+        let bookSelected: Observable<Book> // 사용자가 선택한 책
+    }
+
+    struct Output {
+        let recommendedBooks: Driver<[SectionOfBook]> // 추천된 책 목록
+        let error: Observable<Error> // 에러 처리
+    }
+
+    let disposeBag = DisposeBag()
+
+    let navigateToBookDetail = PublishRelay<Book>()
+
+    func transform(_ input: Input) -> Output {
+        input.viewDidLoad
+            .withUnretained(self)
+            .map { _ in
+                let fetchedBooks = self.bookRepository.fetchBookList(
+                    offset: 0,
+                    limit: 5
+                )
+
+                return [SectionOfBook(items: fetchedBooks)]
+            }
+            .bind(to: recommendedBooksRelay)
+            .disposed(by: disposeBag)
+
+        input.bookSelected
+            .bind(to: navigateToBookDetail) // 책 상세 화면으로 이동
+            .disposed(by: disposeBag)
+
+        return Output(
+            recommendedBooks: recommendedBooksRelay.asDriver(),
+            error: errorRelay.asObservable()
+        )
+    }
+
+    // MARK: Private
+
+    private let bookRepository: BookRepository
+
+    private let recommendedBooksRelay = BehaviorRelay<[SectionOfBook]>(value: [])
+    private let errorRelay = PublishRelay<Error>()
+}
