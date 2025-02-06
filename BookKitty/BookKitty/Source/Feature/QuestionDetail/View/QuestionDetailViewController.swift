247eb4bfb6cf25ac4db0b4119109617a1194cff4
ericKwon95 <ericyong95@gmail.com>
2025-02-06 15:08:52 +0900

    [Feat] QuestionDetail 뷰컨 구현

--- BookKitty/BookKitty/Source/Feature/QuestionDetail/View/QuestionDetailViewController.swift
+++ BookKitty/BookKitty/Source/Feature/QuestionDetail/View/QuestionDetailViewController.swift
@@ -6,7 +6,9 @@
 //  Created by 전성규 on 1/26/25.
 //
 
+import DesignSystem
 import RxCocoa
+import RxDataSources
 import RxSwift
 import SnapKit
 import UIKit
@@ -26,50 +28,239 @@ final class QuestionDetailViewController: BaseViewController {
 
     // MARK: Internal
 
-    let viewModel: QuestionDetailViewModel
-
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
         navigationController?.navigationBar.isHidden = false
     }
 
     override func bind() {
-        let input = QuestionDetailViewModel
-            .Input(testButtonTapTrigger: testButton.rx.tap.asObservable())
+        let input = QuestionDetailViewModel.Input(
+            viewDidLoad: viewDidLoadRelay.asObservable(),
+            deleteButtonTapped: deleteButtonTappedRelay.asObservable(),
+            bookTapped: bookTappedRelay.asObservable()
+        )
 
-        _ = viewModel.transform(input)
+        let output = viewModel.transform(input)
+
+        output.questionDate
+            .drive(onNext: { [weak self] date in
+                self?.dateCaptionLabel.text = date
+                self?.userQuestionHeadlineLabel.text = "\(date), 당신의 질문"
+            })
+            .disposed(by: disposeBag)
+
+        output.userQuestion
+            .drive(onNext: { [weak self] userQuestion in
+                self?.userQuestionBodyLabel.setQuestionText(userQuestion)
+            })
+            .disposed(by: disposeBag)
+
+        output.recommendationReason
+            .drive(onNext: { [weak self] reason in
+                self?.recommendationReasonBodyLabel.text = reason
+            })
+            .disposed(by: disposeBag)
+
+        output.recommendedBooks
+            .drive(recommendedBooksCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+
+        recommendedBooksCollectionView.rx.itemSelected
+            .withLatestFrom(output.recommendedBooks) { indexPath, sectionOfBooks in
+                let books = sectionOfBooks[0].items
+                return books[indexPath.item]
+            }
+            .bind(to: bookTappedRelay)
+            .disposed(by: disposeBag)
+    }
+
+    override func configureNavItem() {
+        let rightBarButtonItem = UIBarButtonItem(
+            title: "삭제",
+            style: .plain,
+            target: self,
+            action: #selector(deleteButtonTapped)
+        )
+        navigationItem.setRightBarButton(rightBarButtonItem, animated: true)
     }
 
     override func configureHierarchy() {
-        [testLabel, testButton].forEach { view.addSubview($0) }
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        [
+            dateCaptionLabel,
+            userQuestionHeadlineLabel,
+            userQuestionBodyLabel,
+            recommendationReasonHeadlineLabel,
+            recommendationReasonBodyLabel,
+            recommendedBooksCollectionView,
+        ].forEach { contentView.addSubview($0) }
     }
 
     override func configureLayout() {
-        testLabel.snp.makeConstraints { $0.center.equalToSuperview() }
-
-        testButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(testLabel.snp.bottom).offset(20.0)
-            $0.width.height.equalTo(150.0)
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
+
+        contentView.snp.makeConstraints { make in
+            make.width.equalToSuperview() // edge가 아닌 width를 맞추는게 뽀인뜨
+            make.verticalEdges.equalToSuperview()
+        }
+
+        dateCaptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView.safeAreaLayoutGuide.snp.top).offset(Vars.spacing24)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(Vars.spacing24)
+        }
+
+        userQuestionHeadlineLabel.snp.makeConstraints { make in
+            make.top.equalTo(dateCaptionLabel.snp.bottom).offset(Vars.spacing4)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(Vars.spacing24)
+        }
+
+        userQuestionBodyLabel.snp.makeConstraints { make in
+            make.top.equalTo(userQuestionHeadlineLabel.snp.bottom).offset(Vars.spacing12)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(Vars.spacing24)
+        }
+
+        recommendationReasonHeadlineLabel.snp.makeConstraints { make in
+            make.top.equalTo(userQuestionBodyLabel.snp.bottom).offset(Vars.spacing48)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(Vars.spacing24)
+        }
+
+        recommendationReasonBodyLabel.snp.makeConstraints { make in
+            make.top.equalTo(recommendationReasonHeadlineLabel.snp.bottom).offset(Vars.spacing8)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide).inset(Vars.spacing24)
+        }
+
+        recommendedBooksCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(recommendationReasonBodyLabel.snp.bottom).offset(Vars.spacing72)
+            make.horizontalEdges.equalTo(contentView.safeAreaLayoutGuide)
+            make.height.greaterThanOrEqualTo(400)
+            make.bottom.equalTo(contentView)
+        }
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        recommendedBooksCollectionView.layoutIfNeeded()
+        recommendedBooksCollectionView.snp.makeConstraints { make in
+            make.height
+                .equalTo(
+                    recommendedBooksCollectionView.collectionViewLayout
+                        .collectionViewContentSize.height
+                )
+        }
+        // content hugging, compression resistance priority도 괜찮은 것 같음
     }
 
     // MARK: Private
 
-    private let testLabel: UILabel = {
-        let label = UILabel()
-        label.text = "P-007"
-        label.font = .systemFont(ofSize: 30.0, weight: .bold)
+    private let bookTappedRelay = PublishRelay<Book>()
 
-        return label
+    private let viewModel: QuestionDetailViewModel
+    private let deleteButtonTappedRelay = PublishRelay<Void>()
+
+    private let scrollView = UIScrollView().then {
+        $0.isScrollEnabled = true
+        $0.showsVerticalScrollIndicator = false
+        $0.showsHorizontalScrollIndicator = false
+    }
+
+    private let contentView = UIView()
+
+    private let dateCaptionLabel = CaptionLabel()
+
+    private let userQuestionHeadlineLabel = Headline3Label(weight: .extraBold).then {
+        $0.textColor = Colors.brandSub
+    }
+
+    private let userQuestionBodyLabel = UserQuestionView(questionText: "")
+
+    private let recommendationReasonHeadlineLabel = Headline3Label(weight: .extraBold).then {
+        $0.text = "추천해요"
+        $0.textColor = Colors.brandSub
+    }
+
+    private let recommendationReasonBodyLabel = BodyLabel()
+
+    private lazy var recommendedBooksCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: makeCollectionViewLayout()
+        )
+        collectionView.backgroundColor = Colors.brandMain30
+        collectionView.register(
+            RecommendedBookCell.self,
+            forCellWithReuseIdentifier: RecommendedBookCell.reuseIdentifier
+        )
+        return collectionView
     }()
 
-    private let testButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Book Thumbnail", for: .normal)
-        button.backgroundColor = .tintColor
+    private let dataSource = RxCollectionViewSectionedReloadDataSource<SectionOfBook>(
+        configureCell: { _, collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: RecommendedBookCell.reuseIdentifier,
+                for: indexPath
+            ) as? RecommendedBookCell else {
+                return RecommendedBookCell(frame: .zero)
+            }
 
-        return button
-    }()
+            cell.configureCell(
+                bookTitle: item.title,
+                bookAuthor: item.author,
+                imageUrl: item.thumbnailUrl?.absoluteString ?? "",
+                isOwned: item.isOwned
+            )
+
+            return cell
+        }
+    )
+
+    @objc
+    private func deleteButtonTapped() {
+        deleteButtonTappedRelay.accept(())
+    }
+}
+
+extension QuestionDetailViewController {
+    private func makeCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(240),
+            heightDimension: .estimated(352)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = Vars.spacing20
+        section.contentInsets = .init(
+            top: Vars.spacing48,
+            leading: Vars.spacing24,
+            bottom: Vars.spacing48,
+            trailing: Vars.spacing24
+        )
+        section.orthogonalScrollingBehavior = .groupPaging
+
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    QuestionDetailViewController(
+        viewModel: QuestionDetailViewModel(
+            questionAnswer: QuestionAnswer(
+                createdAt: Date(),
+                userQuestion: "유저 질문이에요",
+                gptAnswer: "지피티 응답이에요",
+                recommendedBooks: MockBookRepository().mockBookList
+            ),
+            questionHistoryRepository: MockQuestionHistoryRepository()
+        )
+    )
 }
