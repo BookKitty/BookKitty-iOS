8e4c75fd03ead24b469fe44d73c81a7558dfbc9b
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2025-02-11 12:04:36 +0900

    [Refactor] GPTRecommendation -> AiRecommendation (#41)

--- BookKitty/BookKitty/BookMatchKit/Sources/BookMatchCore/Models/AiRecommendation.swift
+++ BookKitty/BookKitty/BookMatchKit/Sources/BookMatchCore/Models/AiRecommendation.swift
@@ -1,27 +1,19 @@
 /// `사용자 질문 기반 도서 추천` 결과를 담는 구조체입니다.
-public struct GPTRecommendationForQuestion: Codable {
-    // MARK: Lifecycle
-
+public struct AiRecommendationForQuestion: Codable {
     public init(ownedBooks: [OwnedBook], newBooks: [RawBook]) {
         self.ownedBooks = ownedBooks
         self.newBooks = newBooks
     }
-
-    // MARK: Public
 
     public let ownedBooks: [OwnedBook]
     public let newBooks: [RawBook]
 }
 
 /// `보유 도서 기반 도서 추천` 결과를 담는 구조체입니다.
-public struct GPTRecommendationFromOwnedBooks: Codable {
-    // MARK: Lifecycle
-
+public struct AiRecommendationFromOwnedBooks: Codable {
     public init(books: [RawBook]) {
         self.books = books
     }
-
-    // MARK: Public
 
     public let books: [RawBook]
 }
