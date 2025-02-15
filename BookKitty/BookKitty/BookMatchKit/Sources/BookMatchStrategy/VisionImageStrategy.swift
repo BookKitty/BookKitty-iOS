14c9cf5e03a3e30f463b59b035b8bb7c0363f8e5
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2025-02-15 17:06:41 +0900

    [Refactor] Strategy들에 대한 반환값 타입 변경 (#66)
    - Single 스트림에서 Double 값으로 변경
    - ImageStrategy 클래스에서 구조체로 변경

--- BookKitty/BookKitty/BookMatchKit/Sources/BookMatchStrategy/VisionImageStrategy.swift
+++ BookKitty/BookKitty/BookMatchKit/Sources/BookMatchStrategy/VisionImageStrategy.swift
@@ -3,27 +3,10 @@ import RxSwift
 import UIKit
 import Vision
 
-/// 도서 표지 이미지 간의 유사도를 계산하는 클래스입니다.
+/// 도서 표지 이미지 간의 유사도를 계산하는 구조체입니다.
 /// Vision 프레임워크를 사용하여 이미지의 특징점을 추출하고 비교합니다.
-public class VisionImageStrategy: SimilarityCalculatable {
-    // MARK: - Nested Types
-
-    // MARK: - Public
-
+public struct ImageVisionStrategy: SimilarityCalculatable {
     public typealias T = UIImage
-
-    // MARK: - Properties
-
-    // MARK: - Private
-
-    private let disposeBag = DisposeBag()
-    private let context = CIContext()
-
-    // MARK: - Lifecycle
-
-    public init() {}
-
-    // MARK: - Functions
 
     /// 두 이미지 간의 `유사도를 계산`합니다.
     ///
@@ -31,25 +14,22 @@ public class VisionImageStrategy: SimilarityCalculatable {
     ///   - image1: 비교할 첫 번째 이미지
     ///   - imageURL2: 비교할 두 번째 이미지의 URL
     /// - Returns: 0부터 100 사이의 유사도 점수 (높을수록 유사)
-    public func calculateSimilarity(_ image1: UIImage, _ image2: UIImage) -> Single<Double> {
-        Single.create { single in
-            let processedImage1 = self.preprocessImage(image1)
-            let processedImage2 = self.preprocessImage(image2)
-
-            do {
-                let featurePrint1 = try self.extractFeaturePrint(from: processedImage1)
-                let featurePrint2 = try self.extractFeaturePrint(from: processedImage2)
-
-                var distance: Float = 0.0
-                try featurePrint1.computeDistance(&distance, to: featurePrint2)
-                let similarity = max(0, min(1, 2.5 - distance * 2.5))
-
-                single(.success(Double(similarity)))
-            } catch {
-                single(.success(Double(-1.0)))
-            }
-
-            return Disposables.create()
+    public static func calculateSimilarity(_ image1: UIImage, _ image2: UIImage) -> Double {
+        let context = CIContext()
+        
+        let processedImage1 = preprocessImage(image1,context)
+        let processedImage2 = preprocessImage(image2,context)
+        
+        do {
+            let featurePrint1 = try extractFeaturePrint(from: processedImage1)
+            let featurePrint2 = try extractFeaturePrint(from: processedImage2)
+            
+            var distance: Float = 0.0
+            try featurePrint1.computeDistance(&distance, to: featurePrint2)
+            
+            return Double(max(0, min(1, 2.5 - distance * 2.5)))
+        } catch {
+            return -1.0
         }
     }
 
@@ -59,7 +39,7 @@ public class VisionImageStrategy: SimilarityCalculatable {
     ///   - image: 특징점을 추출할 이미지
     /// - Returns: 추출된 특징점 데이터
     /// - Throws: BookMatchError.imageCalculationFailed
-    private func extractFeaturePrint(from image: UIImage) throws
+    private static func extractFeaturePrint(from image: UIImage) throws
         -> VNFeaturePrintObservation {
         guard let ciImage = CIImage(image: image) else {
             throw BookMatchError.networkError
@@ -86,7 +66,7 @@ public class VisionImageStrategy: SimilarityCalculatable {
     /// - Parameters:
     ///   - image: 전처리할 이미지
     /// - Returns: 전처리된 이미지
-    private func preprocessImage(_ image: UIImage) -> UIImage {
+    private static func preprocessImage(_ image: UIImage, _ context:CIContext) -> UIImage {
         guard let ciImage = CIImage(image: image) else {
             return image
         }
